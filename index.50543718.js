4bf740375156046b09158fc219ac64e7e5a0642f
dima <shymdima74@gmail.com>
2024-10-15 09:26:26 +0300

    make build

---
+++ index.50543718.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery").addEventListener("click",function(e){document.querySelector(".gallery__large-img").src=e.target.src,e.preventDefault()});
+//# sourceMappingURL=index.50543718.js.map
