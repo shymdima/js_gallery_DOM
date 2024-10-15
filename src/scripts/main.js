23d6af071146cc215f5169acb131ea88733f33f2
dima <shymdima74@gmail.com>
2024-10-15 09:26:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+const changeImage = (eventElement) => {
+  const largePrevious = document.querySelector('.gallery__large-img');
+
+  largePrevious.src = eventElement.target.src;
+  eventElement.preventDefault();
+};
+
+gallery.addEventListener('click', changeImage);
